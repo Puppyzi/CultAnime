115093430e170e634efe357551d74f9f7641d05e
Puppyzi <ayuen699@gmail.com>
2026-06-03 16:51:43 -0400

    bump version to 2.0

--- app/admin/page.js
+++ app/admin/page.js
@@ -10,7 +10,7 @@ const emptyEpisodeEditForm = {
   runtime_minutes: '',
   overview: '',
 };
-const APP_VERSION = '1.9';
+const APP_VERSION = '2.0';
 
 function formatEpisodeDate(value) {
   if (!value) return 'No date';
